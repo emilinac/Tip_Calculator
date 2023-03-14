3217cbdd33797c3d9bc14bc469a804b63252ee72
“Emilina <e.codreanu@students.clark.edu>
2023-03-13 22:31:17 -0400

    tip calculator before styling

--- src/tip_calc.js
+++ src/tip_calc.js
@@ -1,5 +1,39 @@
 // Name: Emilina Sanduleac
 // Date: 3/8/23
-// Description:
+// Description: Tip calculator with percentage and option to split the bill if needed.
+
+'use strict';
+
+const billInput = document.getElementById('billTotalInput')
+const tipInput = document.getElementById('tipInput')
+const numberOfPeopleDiv = document.getElementById('numberOfPeople')
+const perPersonTotalDiv = document.getElementById('perPersonTotal')
+
+let numberOfPeople = Number(numberOfPeopleDiv.innerText)
+
+let calculateBill = () => {
+let bill = Number(billInput.value);
+let tipPercentage = Number(tipInput.value) / 100;
+let tipAmount = bill * tipPercentage;
+let total = bill + tipAmount;
+let perPersonTotal = total / numberOfPeople;
+    perPersonTotalDiv.innerText = `${perPersonTotal.toFixed(2)}`;
+};
+
+let increasePeople = () => {
+numberOfPeople += 1;
+numberOfPeopleDiv.innerText = numberOfPeople;
+    calculateBill()
+};
 
 
+let decreasePeople = () => {
+
+    if (numberOfPeople <= 1) {
+        return
+    }
+numberOfPeople -= 1;
+numberOfPeopleDiv.innerText = numberOfPeople
+    calculateBill()
+
+};
