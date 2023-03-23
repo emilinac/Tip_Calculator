0b0e5357aa78627805443e081eff6b16b9ddf7a1
“Emilina <e.codreanu@students.clark.edu>
2023-03-22 21:36:57 -0400

    tip calculator before styling

--- src/tip_calc.js
+++ src/tip_calc.js
@@ -12,17 +12,17 @@ const perPersonTotalDiv = document.getElementById('perPersonTotal')
 let numberOfPeople = Number(numberOfPeopleDiv.innerText)
 
 let calculateBill = () => {
-let bill = Number(billInput.value);
-let tipPercentage = Number(tipInput.value) / 100;
-let tipAmount = bill * tipPercentage;
-let total = bill + tipAmount;
-let perPersonTotal = total / numberOfPeople;
+    let bill = Number(billInput.value);
+    let tipPercentage = Number(tipInput.value) / 100;
+    let tipAmount = bill * tipPercentage;
+    let total = bill + tipAmount;
+    let perPersonTotal = total / numberOfPeople;
     perPersonTotalDiv.innerText = `${perPersonTotal.toFixed(2)}`;
 };
 
 let increasePeople = () => {
-numberOfPeople += 1;
-numberOfPeopleDiv.innerText = numberOfPeople;
+    numberOfPeople += 1;
+    numberOfPeopleDiv.innerText = numberOfPeople;
     calculateBill()
 };
 
@@ -32,8 +32,8 @@ let decreasePeople = () => {
     if (numberOfPeople <= 1) {
         return
     }
-numberOfPeople -= 1;
-numberOfPeopleDiv.innerText = numberOfPeople
+    numberOfPeople -= 1;
+    numberOfPeopleDiv.innerText = numberOfPeople
     calculateBill()
 
 };
